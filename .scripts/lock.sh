c840b0ba6ab5550322e7336f49b60c3b19ffd262
James Mare <jamesmare12@gmail.com>
2015-11-23 22:09:43 +1100

    added x220 xinitrc, dont mix it up

---
+++ .scripts/lock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+#depends on: imagemagick, i3lock, scrot
+ 
+IMAGE=/tmp/lockscreen.png
+TEXT=/tmp/locktext.png
+ICON=/home/james/.scripts/icons/lock-icon.png
+ 
+scrot $IMAGE
+convert $IMAGE -scale 10% -scale 1000% -fill black -colorize 25% $IMAGE
+
+
+
+
+
+convert $IMAGE $ICON -gravity center -composite -matte $IMAGE
+i3lock -u -i $IMAGE
